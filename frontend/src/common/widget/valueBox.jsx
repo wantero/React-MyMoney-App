0c05bc27ac0d27491b3d58b834a3af3f54369abc
wantero <wantero@dlctec.com.br>
2018-04-08 17:33:12 -0300

    using ValueBox

--- frontend/src/common/widget/valueBox.jsx
+++ frontend/src/common/widget/valueBox.jsx
@@ -3,7 +3,7 @@ import Grid from '../layout/grid'
 
 export default props => (
     <Grid cols={props.cols}>
-        <div className={`small-box bg-${props.colos}`}>
+        <div className={`small-box bg-${props.color}`}>
             <div className='inner'>
                 <h3>{props.value}</h3>
                 <p>{props.text}</p>
